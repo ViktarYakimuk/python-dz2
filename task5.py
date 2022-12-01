04a27e160377a4809d36b3236789221dbb9e7f83
ViktarYakimuk <ViktarYakimuk>
2022-12-01 21:51:31 +0300

    задачa5.

---
+++ task5.py
@@ -0,0 +1,8 @@
+#Реализуйте алгоритм перемешивания списка.
+
+import random
+lst = [random.randint(0,100)
+   for i in range(random.randint(5,20))]
+print(f"исходный список:\n {lst}")
+random.shuffle(lst)
+print(f"список после перемешивания:\n{lst}")
